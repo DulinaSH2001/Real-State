71c4ace7f2a3f01a2ce4e3398bed198b89018f7b
DulinaSH2001 <dulinahejithaindrawansha@gmail.com>
2024-02-07 13:55:53 +0530

    Fix import typo and add Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,23 @@
-import {BrouserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import SignUp from './pages/SignUp';
+import Header from './components/Header';
+import SignIn from './pages/Signin';
 
 export default function App() {
-  return 
-  <BrouserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About/>} />
-      <Route path="/profile" element={<Profile/>} />
-      <Route path="/sign-in" element={<Signin />} />
-      <Route path="/sign-up" element={<SignUp />} />
-      
-   
-    </Routes>
-    </BrouserRouter>
+  return (
+  
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/signin' element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
