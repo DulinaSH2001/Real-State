759874543a7a09780d360d80929800df7dd744a0
DulinaSH2001 <dulinahejithaindrawansha@gmail.com>
2024-02-08 14:09:56 +0530

    Add mongoose dependency and connect to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
 app.listen(3000,()=>{
+    mongoose.connect();
     console.log('server is running on port 3000')
 })
