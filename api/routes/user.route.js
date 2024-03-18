7e53af8b770ab6b57757031fc80d8468a5374bab
DulinaSH2001 <dulinahejithaindrawansha@gmail.com>
2024-03-18 09:36:26 +0530

    create user controller

---
+++ api/routes/user.route.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/test',(req,res) => {
+    res.json({
+        masssage: 'hello world'
+    });
+
+})
+export default router;
